492a798e385aa8b2423b806fdf11deb48ebd9088
vijaykumaru <vijaykumaru@gmail.com>
2018-04-11 08:54:29 +0530

    Add files via upload

---
+++ SubPalindromesInString.java
@@ -0,0 +1,16 @@
+Find all distinct palindromic sub-strings of a given string
+
+Str="Hello";
+HashMap<String, Integer> palindrome = HashMap<>();
+for(i=0;i<n;i++)
+{
+ for(j=1;j<n;j++)
+ {
+	String substring = str.substring(i,j)
+	if(palindrome.get()==null&ispalindrome(substring){
+		palindrome.put(substring,1);
+	}
+ }
+ 
+}
+//Not yet verified inthe system.
